ef4b13fab9e0092eac0d9a6164d6d2a67566d42d
Roshani Gavande <86364356+sgroshani2912@users.noreply.github.com>
2023-06-05 22:22:33 +0530

    Update Kadane's_Algorithm.cpp

--- Kadane's_Algorithm.cpp
+++ Kadane's_Algorithm.cpp
@@ -1,5 +1,23 @@
 // Problem Statement: Given an integer array arr, find the contiguous subarray (containing at least one number) which has the largest sum and returns its sum and prints the subarray.
 
+//Brute Force approach, TC = O(N^2), SC = O(1)
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    long long maxi = 0;
+    
+    for(int i=0; i<n; i++){
+        long long sum = 0;
+        for(int j=i; j<n; j++){
+            sum += arr[j];
+            maxi = max(sum, maxi);
+        }
+        
+    }
+    return maxi;
+}
+
+//Kadane's Algorithm, TC=O(N), SC = O(1)
 #include <bits/stdc++.h> 
 long long maxSubarraySum(int arr[], int n)
 {
