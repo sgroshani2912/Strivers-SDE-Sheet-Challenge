49d1d28a6f258a6f29221740118528369fc4b0cb
Roshani Gavande <86364356+sgroshani2912@users.noreply.github.com>
2023-08-04 18:09:04 +0530

    Create 76_kth_permutation.cpp

---
+++ 76_kth_permutation.cpp
@@ -0,0 +1,24 @@
+// https://rb.gy/778ie
+
+string kthPermutation(int n, int k) {
+    int fact = 1;
+    vector<int> numbers;
+    for(int i=1; i<n; i++){
+        fact *= i;
+        numbers.push_back(i);
+    }
+    numbers.push_back(n);
+
+    string ans = "";
+    k=k-1; // Converting k to 0 based indexing
+
+    while(true){
+        ans+= to_string(numbers[k/fact]);
+        numbers.erase(numbers.begin() + (k/fact));
+        if(numbers.size()==0) break;
+        k = k%fact;
+        fact = fact/numbers.size();
+    }
+
+    return ans;
+}
